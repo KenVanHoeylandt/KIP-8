4e19de0f58446c8b731c270ac336c862ffbfe935
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2017-05-21 22:54:03 +0200

    SoundTimer with javax.sound

--- src/main/kotlin/com/bytewelder/kip8/chip/SoundTimer.kt
+++ src/main/kotlin/com/bytewelder/kip8/chip/SoundTimer.kt
@@ -1,6 +1,11 @@
 package com.bytewelder.kip8.chip
 
 import java.awt.Toolkit
+import javax.sound.midi.MidiSystem
+import javax.sound.midi.MidiChannel
+import javax.sound.midi.Instrument
+
+
 
 /**
  * 60 Hz timer
@@ -8,17 +13,41 @@ import java.awt.Toolkit
 class SoundTimer {
 	val lastTick = System.currentTimeMillis()
 	var value = 0
+	val midiSynth = MidiSystem.getSynthesizer()!!
+
+	init {
+		midiSynth.open()
+		val instruments = midiSynth.defaultSoundbank.instruments
+		midiSynth.loadInstrument(instruments[0])
+	}
 
 	fun update() {
+		val newValue = updateValue()
+
+		if (newValue != value) {
+			updateSynthForValueChange(newValue)
+			value = newValue
+		}
+	}
+
+	private fun updateSynthForValueChange(newValue: Int) {
+		if (newValue > 0) {
+			midiSynth.channels[0].noteOn(60, 100)
+		} else {
+			midiSynth.channels[0].noteOff(60)
+		}
+	}
+
+	private fun updateValue(): Int {
 		if (value > 0) {
 			val current = System.currentTimeMillis()
 			if (current - lastTick >= 17) {
-				value -= 1
+				return value - 1
+			} else {
+				return value
 			}
-		}
-
-		if (value > 0) {
-			Toolkit.getDefaultToolkit().beep()
+		} else {
+			return value
 		}
 	}
 
